8248a36190df324d7cb00b3ab8229b32612dca53
biboca-collab <beatriz.silva.trindade@escola.pr.gov.br>
2026-08-13 08:47:46 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//Botão curtidas
-document.addEventListener
+//Botao curtidas
+document.addEventListener("DOMContentLoaded",()=>{
+    const likeBtn = document.queruSelector(".left-actions .actions-btn:first-child");
+    if (!likeBtn) return;
+    const likeSvg = likeBtn.querySelector("svg");
+
+   //localiza o contador
+
+   let textNode = Array
+})
+
+
